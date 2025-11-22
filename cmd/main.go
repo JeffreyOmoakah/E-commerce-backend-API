38c18819f738639446cf76dfcffe0b2ee8ce6bdb
Jeffrey Omoakah <jomoakah3@gmail.com>
2025-11-22 15:24:18 +0100

    feat: implement full HTTP server with middleware and routing

---
+++ cmd/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log/slog"
+	"os"
+)
+
+func main() {
+	cfg:= config{
+		addr: ":3000",
+		db: dbConfig{},
+	}
+	api := application{
+		config: cfg,
+	}
+	
+	api.run(api.mount())
+	
+	// Logger
+		logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+		slog.SetDefault(logger)
+	
+	
+		if err := api.run(api.mount()); err != nil {
+			slog.Error("server failed to start", "error", err)
+			os.Exit(1)
+		}
+}
