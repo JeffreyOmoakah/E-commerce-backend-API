66ae9e044318e32d6997c64efd70e1d7279bf8e4
Jeffrey Omoakah <jomoakah3@gmail.com>
2025-12-10 13:04:33 +0100

    feat: reformatted codebase and implemented the products service and handler

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,27 +1,44 @@
 package main
 
 import (
+	"context"
 	"log/slog"
 	"os"
 
 	"github.com/JeffreyOmoakah/E-commerce-backend-API/internal/env"
+	"github.com/jackc/pgx/v5"
 )
 
 func main() {
-	cfg:= config{
-		addr: env.GetString("ADDR", ":3000"),
-		db: dbConfig{},
-	}
-	api := application{
-		config: cfg,
-	}
+	ctx := context.Background()
 	
-	api.run(api.mount())
+	cfg:= config{
+		addr: env.GetString("ADDR", ":8080"),
+		db: dbConfig{
+			dsn:env.GetString("GOOSE_DBSTRING", "host=localhost user=postgres password=postgres dbname=ecom sslmode=disable"),
+		},
+	}
 	
 	// Logger
 		logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 		slog.SetDefault(logger)
 	
+	
+	// database 
+	conn, err := pgx.Connect(ctx, cfg.db.dsn)
+	if err != nil {
+		slog.Error("failed to connect to database", "error", err)
+		os.Exit(1)
+	}
+	defer conn.Close(ctx)
+	
+	logger.Info("connected to database","dsn",cfg.db.dsn)
+	
+	api := application{
+		config: cfg,
+	}
+	
+	api.run(api.mount())
 	
 		if err := api.run(api.mount()); err != nil {
 			slog.Error("server failed to start", "error", err)
